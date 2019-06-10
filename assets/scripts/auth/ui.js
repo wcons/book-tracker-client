4b8c073678d6e0b9fe4866db389d6c155359f107
wcons <wconstantino@gmail.com>
2019-06-10 10:19:44 -0400

    Add sign-in events
    Add sign-in events and messaging

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const store = require('../store')
+
 const onSignUpSuccess = responseData => {
   $('#message').text('Signed up successfully!')
   $('#message').removeClass()
@@ -14,7 +16,24 @@ const onSignUpFailure = responseData => {
   $('#sign-up input.formclear').val('')
 }
 
+const onSignInSuccess = responseData => {
+  $('#message').text('Signed in successfully!')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#sign-in input.formclear').val('')
+  store.user = responseData.user
+}
+
+const onSignInFailure = responseData => {
+  $('#message').text('Sign in failed!')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  $('#sign-in input.formclear').val('')
+}
+
 module.exports = {
   onSignUpSuccess,
-  onSignUpFailure
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure
 }
