1220341cc433323e58baa71f32f6c26459e7649c
wcons <wconstantino@gmail.com>
2019-08-06 13:21:45 -0400

    Made change to config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://infinite-reaches-30575.herokuapp.com/',
+  production: 'https://infinite-reaches-30575.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
